ee03eb355534de63417b8d48599ec5e6443579ff
Michelle <flywindy2002@gmail.com>
2016-11-29 01:10:12 +0800

    Register Book, Category, Tag into admin

--- bookshop/books/admin.py
+++ bookshop/books/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book, Category, Tag
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
