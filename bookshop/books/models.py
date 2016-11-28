e719dafdea49d5244f4309e33b4c7a15d8e1f7d4
Michelle <flywindy2002@gmail.com>
2016-11-29 00:48:07 +0800

    Create model Book

--- bookshop/books/models.py
+++ bookshop/books/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    # general fields
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        default='',
+        blank=True
+    )
+    original_price = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+
+    # status and time related fields
+    STATUS_CHOICES = (
+        (0, '未上架'),
+        (1, '上架中'),
+        (2, '已下架')
+    )
+    status = models.PositiveSmallIntegerField(
+        choices=STATUS_CHOICES,
+        default=0
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(null=True)
+
+    # book related fields
+    author = models.CharField(
+        max_length=100,
+        default='',
+        blank=True
+    )
+    translator = models.CharField(
+        max_length=100,
+        default='',
+        blank=True
+    )
+    publisher = models.CharField(
+        max_length=100,
+        default='',
+        blank=True
+    )
+    isbn = models.CharField(
+        max_length=13,
+        default='',
+        blank=True
+    )
+    language = models.CharField(
+        max_length=20,
+        default='',
+        blank=True
+    )
