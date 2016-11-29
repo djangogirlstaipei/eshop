ba06703f13ce003d727a6c6203f8e0643a53decb
Michelle <flywindy2002@gmail.com>
2016-11-29 15:34:23 +0800

    Update verbose_name of model fields

--- bookshop/books/models.py
+++ bookshop/books/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(verbose_name='標籤名稱', max_length=100)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class Tag(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(verbose_name='分類名稱', max_length=100)
 
     def __str__(self):
         return self.name
@@ -25,20 +25,23 @@ class Category(models.Model):
 
 class Book(models.Model):
     # general fields
-    name = models.CharField(max_length=255)
+    name = models.CharField(verbose_name='名稱', max_length=255)
     description = models.TextField(
+        verbose_name='內容簡介',
         default='',
         blank=True
     )
-    original_price = models.PositiveIntegerField(default=0)
-    price = models.PositiveIntegerField(default=0)
+    original_price = models.PositiveIntegerField(verbose_name='原價', default=0)
+    price = models.PositiveIntegerField(verbose_name='售價', default=0)
     category = models.ForeignKey(
         Category,
+        verbose_name='分類',
         null=True,
         blank=True
     )
     tags = models.ManyToManyField(
         Tag,
+        verbose_name='標籤',
         blank=True
     )
 
@@ -49,35 +52,41 @@ class Book(models.Model):
         (2, '已下架')
     )
     status = models.PositiveSmallIntegerField(
+        verbose_name='狀態',
         choices=STATUS_CHOICES,
         default=0
     )
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    published = models.DateTimeField(null=True)
+    created = models.DateTimeField(verbose_name='新增時間', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='更新時間', auto_now=True)
+    published = models.DateTimeField(verbose_name='上架時間', null=True)
 
     # book related fields
     author = models.CharField(
+        verbose_name='作者',
         max_length=100,
         default='',
         blank=True
     )
     translator = models.CharField(
+        verbose_name='譯者',
         max_length=100,
         default='',
         blank=True
     )
     publisher = models.CharField(
+        verbose_name='出版社',
         max_length=100,
         default='',
         blank=True
     )
     isbn = models.CharField(
+        verbose_name='ISBN',
         max_length=13,
         default='',
         blank=True
     )
     language = models.CharField(
+        verbose_name='語言',
         max_length=20,
         default='',
         blank=True
