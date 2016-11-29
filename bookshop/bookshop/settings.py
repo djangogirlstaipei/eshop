4c5b6de475eac22db36ef5cbfd19efcc59723440
Michelle <flywindy2002@gmail.com>
2016-11-29 12:16:18 +0800

    Update LANGUAGE_CODE in settings

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'UTC'
 
