37356de19752e2d3e4019208a4190b2ae56a7500
Michelle <flywindy2002@gmail.com>
2017-02-06 22:04:32 +0800

    Add pagination

--- bookshop/books/views.py
+++ bookshop/books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Book, Category
 
@@ -11,9 +12,19 @@ def book_list_view(request, category=None):
         book_list_queryset = book_list_queryset.filter(
             category__name=category)
 
+    paginator = Paginator(book_list_queryset, 5)
+
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
 
     return render(request, "books/book_list.html",
                   {
-                      'book_list': book_list_queryset,
+                      'book_list': page_obj,
                       'categories': Category.objects.all(),
+                      'page_obj': page_obj
                   })
