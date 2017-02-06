7a80c6371e8a5c6de307594b80c9baf7da4e9d14
Michelle <flywindy2002@gmail.com>
2017-02-06 21:55:00 +0800

    Add category menu

--- bookshop/books/views.py
+++ bookshop/books/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from .models import Book
+
+from .models import Book, Category
 
 
-def home(request):
-    book_list = Book.objects.all()
-    return render(request, 'home.html', {
-        'book_list': book_list,
-    })
+def book_list_view(request, category=None):
+
+    book_list_queryset = Book.objects.all()
+
+    if category:
+        book_list_queryset = book_list_queryset.filter(
+            category__name=category)
+
+
+    return render(request, "books/book_list.html",
+                  {
+                      'book_list': book_list_queryset,
+                      'categories': Category.objects.all(),
+                  })
