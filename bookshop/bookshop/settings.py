a955636822d7cb623dffd5e4d2911a18e62d4338
Michelle <flywindy2002@gmail.com>
2017-02-07 12:55:57 +0800

    Setup assets and use self-hosted bootstrap

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
